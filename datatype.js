54d12b0ab4b2b0d843cf9d58b2ed7f96ffc4911a
Soumyadeep Mukherjee <soumyadeepm2001@gmail.com>
2026-06-25 09:05:00 +0530

    upload datatype file

---
+++ datatype.js
@@ -0,0 +1,17 @@
+/***********************
+ DATATYPES
+ ************************/
+//Primitives
+let name = "John"; // String
+let age = 30; // Number
+let isStudent = false; // Boolean
+let nullValue = null; // Null
+let undefinedValue; // Undefined
+
+//Non-Primitives
+let person = {firstName: "John", lastName: "Doe", age: 30}; // Object
+let numbers = [1, 2, 3, 4, 5]; // Array
+function greet() { // Function
+    console.log("Hello, World!");
+}   
+greet();
