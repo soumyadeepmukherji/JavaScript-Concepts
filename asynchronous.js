a7ee4092beae750a466b38c99a44422e115b1830
Soumyadeep Mukherjee <soumyadeepm2001@gmail.com>
2026-07-01 18:48:13 +0530

    Async-callback hell uploaded

---
+++ asynchronous.js
@@ -0,0 +1,79 @@
+/************************
+ ASYNCHRONOUS OPERATION
+************************/
+
+/*=========================
+    Synchronous execution
+  =========================*/
+//console.log("A")                  // Executed First (A)
+//for(let i = 0; i<100000;i++){}   // Executed Second (wait...)
+//console.log("B")                // Executed third (B)
+
+/*=========================
+    Asynchronous execution
+  =========================*/
+//console.log('A')          //Executed First (A)
+// setTimeout(() => {
+//     console.log('B');   // Executed at last (B)
+// },0)
+//console.log('C')       // Executed second (C)
+
+/*=======Need Of Callback function in async=======*/
+// function getData(){
+//     setTimeout(()=>{
+//         return [{},{}]
+//     },0)
+// }
+// const result = getData()
+// console.log(result); // undefined
+
+// function getRes(Callback){
+//     setTimeout(()=>{
+//         let data = [{},{}]
+//         Callback(data)
+//     },0)
+// }  
+// getRes((data) => console.log(data)) // [ {}, {} ]
+
+/*====Callback Hell=====*/
+// function login(Callback){
+//     setTimeout(() => {
+//         console.log("user logged in")
+//         Callback()
+//     },1000)
+// }
+
+// function getProfile(Callback){
+//     setTimeout(() => {
+//         console.log("Profile fetched")
+//         Callback()
+//     },1000)
+// }
+
+// function getOrder(Callback){
+//     setTimeout(() => {
+//         console.log("Order fetched")
+//         Callback()
+//     },1000)
+// }
+
+// function payment(Callback){
+//     setTimeout(() => {
+//         console.log("payment Done")
+//         Callback()
+//     },1000)
+// }
+
+// login(() => {
+//     getProfile(() => {
+//         getOrder(() => {
+//             payment(() =>{
+//                 console.log('Completed')
+//             })
+//         })
+//     })
+// })
+
+/*******************************************************
+ To over come the callback hell we are will go with Promise
+ ******************************************************/
